622a18b054deb5bb67169292c25f2ca5f500819e
Rich Howell <gustove@gmail.com>
2020-07-18 11:46:47 -0400

    fix rounding bug

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -14,10 +14,12 @@ import kettle from '../../assets/images/kettle-line.svg';
 import { AppState, getDefaultState, PourOverType } from '../../state';
 import { RatioSlider } from './RatioSlider';
 
-const FlexRight = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  width: 90vw;
+const SettingsButton = styled(IconButton)`
+  && {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  }
 `;
 
 interface SettingsProps {
@@ -40,39 +42,37 @@ export const Settings: FC<SettingsProps> = ({ state, onChange }) => {
   };
   return (
     <>
-      <FlexRight>
-        <IconButton onClick={() => setIsOpen(true)}>
-          <SettingsIcon />
-        </IconButton>
-      </FlexRight>
+      <SettingsButton onClick={() => setIsOpen(true)}>
+        <SettingsIcon />
+      </SettingsButton>
+
       <Dialog open={isOpen} onClose={() => setIsOpen(false)} fullWidth>
-        <DialogTitle></DialogTitle>
         <DialogContent>
-          <Typography variant="h4">
+          <Typography variant="h4" style={{ marginTop: '32px' }}>
             <img alt="Hot Coffee" height="30" src={hotSvg} /> Hot Coffee
           </Typography>
 
           <RatioSlider ratios={ratios} onChange={onChange} type={PourOverType.Hot} />
 
-          <Divider style={{ margin: '16px 0 32px 0' }} />
-          <Typography variant="h4">
+          <Divider style={{ margin: '16px 0 0 0' }} />
+          <Typography variant="h4" style={{ marginTop: '32px' }}>
             <img alt="Iced Coffee" height="30" src={icedSvg} /> Iced Coffee
           </Typography>
 
           <RatioSlider ratios={ratios} onChange={onChange} type={PourOverType.Iced} />
           <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '16px' }}>
             <div>
-              <img alt="Water" height="20" src={kettle} /> Water {100 - icePercent}%
+              <img alt="Water" height="20" src={kettle} /> Water {(100 - icePercent).toFixed(0)}%
             </div>
             <div>
-              <img alt="Ice" height="20" src={iceSvg} /> Ice {icePercent}%
+              <img alt="Ice" height="20" src={iceSvg} /> Ice {icePercent.toFixed(0)}%
             </div>
           </div>
           <Slider value={icePercent} onChange={(_, value) => setIceRate(value as number)} min={25} max={55} />
         </DialogContent>
         <DialogActions>
-          <Button onClick={reset}>Reset to Default</Button>
-          <Button onClick={() => setIsOpen(false)}>Ok</Button>
+          <Button onClick={reset}>Reset to Defaults</Button>
+          <Button onClick={() => setIsOpen(false)}>Done</Button>
         </DialogActions>
       </Dialog>
     </>
